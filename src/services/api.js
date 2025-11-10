c1a7fc0c5fabfa0a156bf2a164b36bee74c6450d
MateoDelgadoU <mateldraka@gmail.com>
2025-11-10 19:34:41 -0300

    push de api.js tras hacer un merge

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../store/authStore";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://localhost:5000/api", //Poner esto en un .env
   headers: {
     "Content-Type": "application/json",
   },
